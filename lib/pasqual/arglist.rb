23f8772b7d80d58247e26d690d46b0fe3f9d2d49
Derek Kastner <dkastner@gmail.com>
2014-11-29 15:41:09 -0600

    Don't pass psql params if arguments are nil

--- lib/pasqual/arglist.rb
+++ lib/pasqual/arglist.rb
@@ -3,8 +3,11 @@ module Pasqual
   module Arglist
 
     def self.args(username, password, host, port, name)
-      list = ['-h', host, '-U', username, '-p', port.to_s]
-      list += ['-w'] if password.nil?
+      list = []
+      list << '-h' << host if host.to_s.length > 0
+      list << '-U' << username if username.to_s.length > 0
+      list << '-p' << port.to_s if port.to_s.length > 0
+      list << '-w' if password.to_s.length == 0
 
       list + [name]
     end
