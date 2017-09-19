b04871feafbe297a1897dd5db041574d6b528053
Seamus Abshere <seamus@abshere.net>
2017-09-19 15:41:58 -0400

    don't listen to psqlrc or paging

--- lib/pasqual/arglist.rb
+++ lib/pasqual/arglist.rb
@@ -8,6 +8,8 @@ module Pasqual
       list << '-U' << username if username.to_s.length > 0
       list << '-p' << port.to_s if port.to_s.length > 0
       list << '-w' if password.to_s.length == 0
+      list << '--no-psqlrc'
+      list << '--pset' << 'pager=off'
 
       list + [name]
     end
